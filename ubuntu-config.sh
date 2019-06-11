22e1f64a47f091859e774c3fefd485b0e5c247d2
fiberoptix <andrew.gamache@gmail.com>
2019-06-11 17:14:56 -0400

    Merge branch 'master' of https://github.com/fiberoptix/bash-scripts
    # Conflicts:
    #	if_then_case.sh

--- ubuntu-config.sh
+++ ubuntu-config.sh
@@ -12,11 +12,25 @@ yes | sudo apt install openjdk-8-jdk
 # Install newest Java 10 - Jenkins incompatible with this-- needs 8 above
 # yes | sudo apt-get install default-jdk
 
-# create ssh keys for trusted connect
-mkdir .ssh; sleep 3; ssh-keygen -f .ssh/id_rsa -t rsa -N ''
+# create ssh files and directories
+cd ~
+mkdir .ssh
+sleep 3
+chmod 700 .ssh
+
+touch .ssh/authorized_keys
+chmod 600 .ssh/authorized_keys
+
+# create ssh keys for trusted connect  don't add a pass phrase or it will prompt
+ssh-keygen -f .ssh/id_rsa -t rsa -N ''
 
 #setup a file or variable for target hosts that will get a public.key
 #touch ~/rsa_hosts.txt
 #for ip in `cat ~/rsa_hosts.txt`; do
-#    yes yes | sudo ssh-copy-id -i ~/.ssh/id_rsa.pub $ip
+
+# copy the publickeyt o the remote host
+#    yes yes | sudo ssh-copy-id -i ~/.ssh/id_rsa.pub ubuntu@$ip
+
+# test access - should conenct without a password
+# ssh -i ~/.ssh/mykey user@host
 #done
