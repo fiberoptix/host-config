e4b04917dc18c5ae10f6526316c1c6976171a34f
fiberoptix <andrew.gamache@gmail.com>
2019-06-27 12:10:32 -0400

    Merge branch 'master' of https://github.com/fiberoptix/bash-scripts
    # Conflicts:
    #	if_then_case.sh

--- ansible-config.sh
+++ ansible-config.sh
@@ -17,18 +17,13 @@ sudo ssh-keygen -f .ssh/id_rsa -t rsa -N ''
 
 #Exchange RSA keys from Master to Nodes
 # You're going to have to type in the password for all of these
-for host in 172.31.22.36 172.31.22.244 172.31.25.70; do
+for host in 172.31.60.98 172.31.60.232 172.31.60.179; do
 ssh-copy-id $host;
 done
 
 # Prep and install Ansible
-sudo yum -y install python3-pip
-sudo pip3 install --upgrade pip
 pip3 install ansible --user
 
-#Check out pyton and ansible
+#Check out python and ansible
 sudo yum list installed | grep python
 ansible --version
-
-
-
--- ansible-preconfig.sh
+++ ansible-preconfig.sh
@@ -7,7 +7,8 @@
 #yes 'newpassword' | sudo passwd root
 
 #Change the hostname
-hostnamectl set-hostname Ansible-Master
+HOSTNAME=Ansible-$1
+hostnamectl set-hostname $HOSTNAME
 
 #Or use this and pass it a hostname when you run the script
 #hostnamectl set-hostname Ansible-$1
@@ -35,20 +36,13 @@ systemctl restart sshd
 # RHEL Install Enterprise extensions
 sudo yum install wget -y
 cd /etc/yum.repos.d
-
 sudo rpm -Uvh https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm
 sudo yum update --nobest -y
 
-sudo yum -y install ansible
+# install Python3 and PIP
+sudo yum install python3 -y
+sudo yum -y install python3-pip -y
+sudo pip3 install --upgrade pip
 
-# wget https://dl.fedoraproject.org/pub/epel/7/x86_64/Packages/e/epel-release-7-11.noarch.rpm
-# rpm -ivh epel-release-7-11.noarch.rpm
-
-# UBUNTU
-# sudo apt update
-# sudo apt install software-properties-common
-# sudo apt-add-repository --yes --update ppa:ansible/ansible
-# sudo apt install ansible
-
-
-
+#Check out python and ansible
+sudo yum list installed | grep python
