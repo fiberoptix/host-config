de422f33e0b55b2e3ca1b98b4745f051c9bab8b8
fiberoptix <andrew.gamache@gmail.com>
2019-07-02 13:07:24 -0400

    Merge branch 'master' of https://github.com/fiberoptix/bash-scripts
    # Conflicts:
    #	if_then_case.sh

--- ansible-master-install.sh
+++ ansible-master-install.sh
@@ -34,7 +34,20 @@ sudo chown -R ansible:ansible /home/ansible/.ssh
 # consider using this sshpass solution in the future
 # https://serverfault.com/questions/306541/automating-ssh-copy-id
 #
-for host in 172.31.58.61 172.31.48.204 172.31.60.203; do
+# TODO: remember to update this HOST IP file in the repository
+wget https://raw.githubusercontent.com/fiberoptix/ansible-dev/master/project.hosts
+while read -r host do
 ssh-copy-id -i /home/ansible/.ssh/id_rsa.pub $host;
-done
+done < project.hosts
 
+#Setup Ansible Config Files
+sudo mkdir /etc/ansible
+sudo chown -R ansible:ansible /etc/ansible
+chmod -R 775 /etc/ansible
+
+cd /etc/ansible/
+# TODO: remember to update this file with the default ansible config and pre-commit to repo
+wget https://raw.githubusercontent.com/fiberoptix/ansible-dev/master/ansible.cfg
+
+# TODO: remember to update this ANSIBLE HOST file in the repository
+wget https://raw.githubusercontent.com/fiberoptix/ansible-dev/master/hosts
